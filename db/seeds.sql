ad3924809e5c4998be4158234ed50e15cf0f0f1c
Angela Man <angela.k.man@gmail.com>
2021-06-15 12:28:14 -0500

    created log in and sign up pages

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO user ('username', 'password')
+VALUES
+    ('user1', 'pw123'),
+    ('user2', 'pw1234'),
+    ('user3', 'pw123');
+
+INSERT INTO post ('title', 'post_text', 'user_id')
+VALUES
+    (),
+    (),
+    ();
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const routes = require('./controllers/api/index');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// set up Handlebar.js as template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 // turn on routes
 app.use(routes);
 
-// connect databse to server
+// connect database to server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
